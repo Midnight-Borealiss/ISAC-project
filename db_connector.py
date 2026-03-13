946852144c46c674dfbaaf53e7b2d1c611e25ac5
app-Encas <votre.email@example.com>
2026-03-13 08:52:32 +0000

    fix: resolution erreur SSL et ajout dnspython

--- db_connector.py
+++ db_connector.py
@@ -21,21 +21,27 @@ class MongoDBConnector:
 
         try:
             status_placeholder.write("🔌 Tentative de connexion au Cluster...")
-            # serverSelectionTimeoutMS est crucial pour ne pas bloquer l'interface
-            self.client = MongoClient(self.uri, serverSelectionTimeoutMS=5000)
+            
+            # C'EST ICI QU'ON MODIFIE LES PARAMÈTRES
+            self.client = MongoClient(
+                self.uri, 
+                serverSelectionTimeoutMS=5000,
+                connectTimeoutMS=10000,
+                tls=True,
+                retryWrites=True
+            )
             
             # Vérification réelle de la connexion
             self.client.admin.command('ping') 
             
             self.db = self.client['isac_db']
-            status_placeholder.empty() # On efface les logs de debug si ça réussit
+            status_placeholder.empty()
             st.sidebar.success("✅ Base de données connectée")
             
         except Exception as e:
             status_placeholder.empty()
             st.error(f"❌ Erreur réseau MongoDB : {e}")
             self.client = None
-
     def get_collection(self, collection_name):
         if self.client:
             return self.db[collection_name]
@@ -44,6 +50,8 @@ class MongoDBConnector:
     def close_connection(self):
         if self.client:
             self.client.close()
+
+
 
 # Singleton : Une seule connexion pour toute l'app
 if 'db_connector' not in st.session_state:
